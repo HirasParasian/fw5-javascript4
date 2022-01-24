411364af33bbe3f27030ce7586a29bee7e77564b
HirasParasian <hirasparasian@gmail.com>
2022-01-24 14:35:12 +0700

    revisi sikit

--- divideandsort.js
+++ divideandsort.js
@@ -4,7 +4,7 @@ function divedeAndSort(inputNumber) {
         return "Harus Angka"
     }
     let hasil = ""
-    let array = inputNumber.toString()
+    let array = inputNumber.toString() //convert ke string
     let strArray = array.split('0'); // split setiap ada 0
     for (let i = 0; i < strArray.length; i++) { //perulangan
         let splitArray = strArray[i].split('')  //split array per index
@@ -15,7 +15,7 @@ function divedeAndSort(inputNumber) {
         // console.log(joinArray)
         hasil = hasil + joinArray //menggabungkan
     }   
-    return Number(hasil) 
+    return Number(hasil) //convert ke number 
 }
 console.log(divedeAndSort(5956560159466056))
 console.log(divedeAndSort("5956560159466056"))
